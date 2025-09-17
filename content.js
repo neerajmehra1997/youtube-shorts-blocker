a7ead87f580a8e2e9a4d620d407b95050cb097d4
neerajsingh <neerajmehra1997@gmail.com>
2025-09-18 03:01:46 +0530

    constant created for youtube shorts keywords

--- content.js
+++ content.js
@@ -1,13 +1,18 @@
+let shortsKeywords = [
+    'ytd-reel-shelf-renderer',
+    'ytm-shorts-lockup-view-model-v2',
+    'a[href*="/shorts/"]',
+    'ytd-reel-player-renderer'
+]
+
 function removeShorts() {
+    const selector = shortsKeywords.join(', ');
     document.querySelectorAll(
-        'ytd-reel-shelf-renderer, ' +
-        'ytm-shorts-lockup-view-model-v2, ' +
-        'a[href*="/shorts/"], ' +
-        'ytd-reel-player-renderer'
+        selector
     ).forEach(el => el.remove());
 
-    // 2. If user is on the Shorts dedicated page, redirect them to home
-    if (window.location.pathname.startsWith("/shorts/*")) {
+    // If user is on the Shorts dedicated page, redirect them to home
+    if (window.location.pathname.startsWith("/shorts")) {
         console.log("Redirecting from Shorts page to Home...");
         window.location.href = "https://www.youtube.com/";
     }
